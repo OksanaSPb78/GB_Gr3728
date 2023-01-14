432328f96a73954ac24fc10b6ff2a9eb662c3872
Oksana Smirnova <gorbunova268@mail.ru>
2023-01-15 00:10:56 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача №3
+// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+
+//  Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+// Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    // ВАРИАНТ 1
+    //     // Создаем массив с количеством дней недели
+    //     string[]dayWeek = new string[7];
+    //     dayWeek[0] = "Monday";
+    //     dayWeek[1] = "Tuesday";
+    //     dayWeek[2] = "Wednesday";
+    //     dayWeek[3] = "Thursday";
+    //     dayWeek[4] = "Friday";
+    //     dayWeek[5] = "Saturday";
+    //     dayWeek[6] = "Sunday";
+    //     // Парсим введенное число
+    //     int inputNum = int.Parse(inputLine);
+    //     // Находим название дня недели по введенному номеру
+    //     string dayofWeek = dayWeek[inputNum -1];
+    //     // Выводим данные в консоль
+    //     Console.WriteLine(dayofWeek);
+
+    // }
+    // ВАРИАНТ 2
+    int inputNumber = int.Parse(inputLine);
+    // string outDayOfWeek = string.Empty;
+    //  switch (inputNumber)
+    // {
+    //     case 1: outDayOfWeek = "понедельник"; break;
+    //     case 2: outDayOfWeek = "вторник"; break;
+    //     case 3: outDayOfWeek = "среда"; break;
+    //     case 4: outDayOfWeek = "четверг"; break;
+    //     case 5: outDayOfWeek = "пятница"; break;
+    //     case 6: outDayOfWeek = "суббота"; break;
+    //     case 7: outDayOfWeek = "воскресенье"; break;
+    //     default: outDayOfWeek = "это не день недели!"; break;
+    // }
+    // Console.WriteLine(outDayOfWeek);
+    // ВАРИАНТ 3
+    string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+    Console.WriteLine(outDayOfWeek);
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,48 @@
+﻿//    Задача №5
+// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+//Считываем данные с консоли 
+// string? imputNumLine = Console.ReadLine();
+// // Проверяем, чтобы данные не были пустыми
+// if (imputNumLine != null)
+// {
+//     // Парсим введенное число
+//     int imputNumberA = int.Parse(imputNumLine);
+//     // Вводим промежуточное значение
+//     int imputNumberC = imputNumberA * (-1);
+//     // Зоздаем выходную переменную
+//     string outNumLine = string.Empty;
+//     // Запускаем цикл (пока)
+//     while (imputNumberC < imputNumberA)
+//     {
+//         outNumLine = outNumLine + imputNumberC + ",";
+//         imputNumberC = imputNumberC + 1;
+//     }
+//     // Добавляем значение, который будет не достигнут
+//     outNumLine = outNumLine + imputNumberA;
+//     // Вывод значения пользователю
+//     Console.WriteLine(outNumLine);
+// }
+
+// Вариант Савелия 
+// Приглашение ввести число
+Console.Write("input your num: ");
+// Вводим число (считываем данные с консоли)
+string? inputNum = Console.ReadLine();
+// Проверяем это число, чтобы оно не было нулевым (данные не пустые)
+if (inputNum != null)
+{
+    // Преобразуем (парсим) это число в значение
+    int num = int.Parse(inputNum);
+    // Цикл for (выполняет указанный код множество раз)
+    for (int i = 0; i < num * 2; i++)
+    {
+        // Вывод каждого числа отдельно
+        Console.Write(i - num);
+        // Дописываем "," к каждой строке
+        Console.Write(", ");
+    }
+    Console.Write(num);
+}
